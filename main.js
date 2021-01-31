09957d71d5daf703af2ab2b3966f0076cc7ad0f4
Luc Burns <burnsluc2@gmail.com>
2021-01-30 21:24:11 -0500

    Query the dom

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+// Query the DOM
+
+let playerScore = document.getElementById('player-score');
+let computerScore = document.getElementById('computer-score');
+
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissor = document.getElementById('scissor');
+
